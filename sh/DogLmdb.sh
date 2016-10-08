b0f5de6175cb105c3f517e072051d2147d3f29be
bbfamily <bbfamily@126.com>
2016-10-08 23:19:30 +0800

    match deploy 256

--- sh/DogLmdb.sh
+++ sh/DogLmdb.sh
@@ -1,6 +1,5 @@
 # /usr/bin/env sh
 echo "Begin..."
-
 ROOTFOLDER=../gen/baidu/image/
 OUTPUT=../gen/dog_judge
 
@@ -12,8 +11,7 @@ $ROOTFOLDER $OUTPUT/train_split.txt  $OUTPUT/img_train_lmdb
 rm -rf $OUTPUT/img_val_lmdb
 /Users/Bailey/caffe/build/tools/convert_imageset --shuffle \
 --resize_height=256 --resize_width=256 \
-$ROOTFOLDER $OUTPUT/val_split.txt  $OUTPUT/img_val_lmdb
-
+$ROOTFOLDER $OUTPUT/val_split.txt  $OUTPUT/img_val_lmdb d
 echo "Done.."
 
 
