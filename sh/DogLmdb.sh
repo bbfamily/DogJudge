2f683bc4a28130036093056eb5e7df8b90527a09
bbfamily <bbfamily@126.com>
2016-10-08 23:19:04 +0800

    match deploy 256

--- sh/DogLmdb.sh
+++ sh/DogLmdb.sh
@@ -6,12 +6,12 @@ OUTPUT=../gen/dog_judge
 
 rm -rf $OUTPUT/img_train_lmdb
 /Users/Bailey/caffe/build/tools/convert_imageset --shuffle \
---resize_height=224 --resize_width=224 \
+--resize_height=256 --resize_width=256 \
 $ROOTFOLDER $OUTPUT/train_split.txt  $OUTPUT/img_train_lmdb
 
 rm -rf $OUTPUT/img_val_lmdb
 /Users/Bailey/caffe/build/tools/convert_imageset --shuffle \
---resize_height=224 --resize_width=224 \
+--resize_height=256 --resize_width=256 \
 $ROOTFOLDER $OUTPUT/val_split.txt  $OUTPUT/img_val_lmdb
 
 echo "Done.."
